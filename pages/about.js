0ae8236b3dd13bfbc9da19faad611b9fcc6558b1
zereight <aldwk68@naver.com>
2019-04-13 13:27:56 +0900

    1. init & Head & Link
    init next/ using Link/ using Head

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Header from "../components/Header";
+
+export default () => (
+    <div>
+        <Head>
+            <title>About | My Store</title>
+            <Header/>
+        </Head>
+        <h1>About Page</h1>
+    </div>
+)
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Link from "next/link";
+import Header from "../components/Header";
+
+
+export default () => (
+    <div>
+        <Head>
+            <title>Home | My Store</title>
+            <Header/>
+        </Head>
+
+        <h1>This is main page.</h1>
+        
+    </div>
+);
